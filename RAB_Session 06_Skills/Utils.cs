b43110d9b164e70de68e7c0209e1a6e2f790273e
Tom E Flaherty <teflaherty@utexas.edu>
2023-01-24 06:01:23 -0600

    Add project files.

---
+++ RAB_Session 06_Skills/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAB_Session_06_Skills
+{
+    internal static class Utils
+    {
+        internal static void method1()
+        {
+            // add method code here
+        }
+    }
+}
