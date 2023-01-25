6f85f48c6247e16778ccd9ec4e866089e768c112
Tom E Flaherty <teflaherty@utexas.edu>
2023-01-25 15:25:07 -0600

    step 7 complete

--- RAB_Session 06_Skills/App.cs
+++ RAB_Session 06_Skills/App.cs
@@ -22,9 +22,9 @@ namespace RAB_Session_06_Skills
 
             // step 2: create ribbon panel(s)
 
-            a.CreateRibbonPanel("Revit Add-in Bootcamp", "Panel 1");
-            a.CreateRibbonPanel("Revit Add-in Bootcamp", "Panel 2");
-            a.CreateRibbonPanel("Panel 3");
+            RibbonPanel panel1 = a.CreateRibbonPanel("Revit Add-in Bootcamp", "Panel 1");
+            RibbonPanel panel2 = a.CreateRibbonPanel("Revit Add-in Bootcamp", "Panel 2");
+            RibbonPanel panel3 = a.CreateRibbonPanel("Panel 3");
 
             // step 3: create button data instances
 
@@ -34,6 +34,10 @@ namespace RAB_Session_06_Skills
             PushButtonData pData4 = new PushButtonData("button4", "Button 4", assemblyName, "RAB_Session_06_Skills.cmdCommand2");
             PushButtonData pData5 = new PushButtonData("button5", "Button 5", assemblyName, "RAB_Session_06_Skills.cmdCommand1");
             PushButtonData pData6 = new PushButtonData("button6", "This is \rButton 6", assemblyName, "RAB_Session_06_Skills.cmdCommand2");
+
+            PulldownButtonData pullDownData1 = new PulldownButtonData("pulldown1", "Pulldown Button");
+
+            SplitButtonData splitData1 = new SplitButtonData("split1", "Split Button");
 
             // step 4: add images
 
@@ -52,7 +56,23 @@ namespace RAB_Session_06_Skills
 
             // step 5: add tool tips
 
-            // step 6: create buttons         
+            pData1.ToolTip = "Button 1 tool tip";
+            pData2.ToolTip = "Button 2 tool tip";
+
+            // step 6: create buttons
+
+            panel1.AddItem(pData1);
+            panel1.AddItem(pData2);
+
+            SplitButton split1 = panel1.AddItem(splitData1) as SplitButton;
+            split1.AddPushButton(pData3);
+            split1.AddPushButton(pData4);
+
+            PulldownButton pull1 = panel2.AddItem(pullDownData1) as PulldownButton;
+            pull1.AddPushButton(pData5);
+            pull1.AddPushButton(pData6);
+
+            panel3.AddStackedItems(pData1, pData2, pData3);
 
             return Result.Succeeded;
         }
